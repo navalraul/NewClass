87439b45639d47d2e88bfac6d3ea05f8d2eadd0c
navalraul <129669942+navalraul@users.noreply.github.com>
2023-06-20 16:01:02 +0530

    update

---
+++ Day30.js
@@ -0,0 +1,39 @@
+var array = [ { id: 1, name: "Naval", age: 23}, { id: 2, name: "kunal", age: 24}, 
+{ id: 3, name: "Navnath", age: 25,}, { id: 4, name: "rahul", age: 26}]
+
+array.map ((object) => {
+    console.log(object)
+})
+
+
+var userage = 24;
+
+
+var selectedUser = array.find((object) => object.age == userage);
+console.log(selectedUser)
+
+var userId = 4;
+
+
+var selectedUser = array.find((object) => object.id == userId)
+console.log(selectedUser)
+
+
+
+
+var userName = 'Nakck';
+
+var index = array.findIndex((object) => object.name == userName);
+console.log(index)
+
+
+var numbers = [ 1,9,8,5,6,2,7,3,4];
+console.log(numbers.sort((a,b) => a-b))
+
+console.log(numbers.sort((a,b) => b-a))
+
+
+const words = [ 'spray', 'limit', 'elite', 'exiirijkv', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6);
+console.log(result)
